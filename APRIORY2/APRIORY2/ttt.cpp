71637ab419c857f0d39ad4be6dde82871726b213
Bernardov <funnynick@yandex.ru>
2013-10-28 22:51:46 +0200

    code from other algoritm
    In this algoritm loading data same as in algoritm of Naive Byas
    Classifier

---
+++ APRIORY2/APRIORY2/ttt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ifstream f("fff.txt");  
+	while(!f.eof())
+	{
+		
+	}
+	f.close();
+
+	apriory(arr);
+}
