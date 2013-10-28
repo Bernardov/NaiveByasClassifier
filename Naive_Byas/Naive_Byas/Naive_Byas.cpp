62ed0a59a426e22e28ec3cbe527d9350e5684904
Bernardov <funnynick@yandex.ru>
2013-10-28 22:36:05 +0200

    start_t
    first part of aplication

---
+++ Naive_Byas/Naive_Byas/Naive_Byas.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "fstream"
+
+using namespace std;
+
+typedef int T;
+
+struct note
+{
+	T age;
+	T salary;
+	bool is_stud;
+	T cred_reit;
+	bool sale;
+}
+
+bool processing(note* list, int ind, T age, T salary, bool is_stud, T cred_rait)
+{
+	double p_y[5];
+	double p_n[5];
+
+}
+
+int main()
+{
+	note list[5];
+	list[1].age=1;
+
+	system("pause");
+	return 0;
+}
